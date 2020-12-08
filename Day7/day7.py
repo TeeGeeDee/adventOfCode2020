e90bdbf0ffbedc3ca19afb3e25056d008ee8180b
Tristan Gray-Davies <tgraydavies@gmail.com>
2020-12-08 23:34:26 +0000

    tidy day 7 and 8 solutions

--- Day7/day7.py
+++ Day7/day7.py
@@ -1,22 +1,18 @@
 
+from typing import List
 from itertools import chain
 
-def find_bags_im_in(bag_map: dict,colour: str):
+def find_containers(bag_map: dict,colour: str):
     contains_colour = [b for b in bag_map if colour in bag_map[b]]
-    inherited = list(chain(*[find_bags_im_in(bag_map,c) for c in contains_colour]))
+    inherited = list(chain(*[find_containers(bag_map,c) for c in contains_colour]))
     return list(set(contains_colour + inherited))
 
 def count_bags(bag_map: dict,colour: str):
     this_bag = bag_map[colour]
     return sum([this_bag[c]*(1+count_bags(bag_map,c)) for c in this_bag])
     
-
-if __name__ == '__main__':
-    
-    with open("data.txt", "r") as f:
-        rules = [r.rstrip('\n') for r in f.readlines()]
+def rules_to_map(rules: List[str]):
     rules = [r.replace(' bags.','').replace(' bag.','') for r in rules]
-    
     bag_map = dict()
     for r in rules:
         container,contains = r.split(' bags contain ')
@@ -26,8 +22,16 @@ if __name__ == '__main__':
             contains = []
         contains = {b[b.find(' ')+1:]:int(b[:b.find(' ')]) for b in contains}
         bag_map[container] = contains
+    return bag_map
 
-    tgd = find_bags_im_in(bag_map,'shiny gold')
-    print('ans 1 = {0}'.format(len(find_bags_im_in(bag_map,'shiny gold'))))
+
+if __name__ == '__main__':
+    
+    with open("data.txt", "r") as f:
+        rules = [r.rstrip('\n') for r in f.readlines()]
+
+    bag_map = rules_to_map(rules)
+
+    print('ans 1 = {0}'.format(len(find_containers(bag_map,'shiny gold'))))
     print('ans 2 = {0}'.format(count_bags(bag_map,'shiny gold')))
     
--- Day8/day8.py
+++ Day8/day8.py
@@ -3,23 +3,35 @@ from typing import List
 
 def run_code(code: List[str]):
     acc = 0
-    pos = 0
+    i = 0
     visited = set()
-    reached_end = False
-    while pos not in visited:
-        visited.update([pos])
-        action,n = code[pos][:3],int(code[pos][4:])
+    while (i not in visited) and (i<len(code)):
+        visited.add(i)
+        action,n = code[i][:3],int(code[i][4:])
         if action == 'acc':
             acc += n
-            pos += 1
+            i += 1
         elif action == 'jmp':
-            pos += n
+            i += n
         elif action == 'nop':
-            pos += 1
-        if pos==len(code):
-            reached_end = True
-            break
+            i += 1
+
+        reached_end = i>=len(code)
     return reached_end, acc
+
+def fix_and_run_code(code: List[str]):
+    i = 0
+    finished = False
+    action_switch = {'jmp':'nop','nop':'jmp'}
+    while not finished:
+        action = code[i][0:3]
+        if action in action_switch:
+            mod_code = code.copy()
+            mod_code[i] = mod_code[i].replace(action,action_switch[action])
+            finished,acc = run_code(mod_code)
+        i += 1
+    return acc
+
 
 if __name__ == '__main__':
     
@@ -27,16 +39,5 @@ if __name__ == '__main__':
         code = [c.rstrip('\n') for c in f.readlines()]
         
     print('before revisiting a previous state acc was {0}'.format(run_code(code)[1]))
-    line = 0
-    finished = False
-    while not finished:
-        mod_code = code.copy()
-        if code[line][0:3]=='jmp':
-            mod_code[line] = mod_code[line].replace('jmp','nop')
-        if code[line][0:3]=='nop':
-            mod_code[line] = mod_code[line].replace('nop','jmp')
-        if code[line][0:3]=='jmp' or code[line][0:3]=='nop':
-            finished,acc = run_code(mod_code)
-        line += 1
-    print('after fixing the code, at the end acc = {0}'.format(acc))
+    print('after fixing the code, at the end acc = {0}'.format(fix_and_run_code(code)))
             
