ba87a28e1cd1d1d3b1826625df18e83c22ad315b
Tristan Gray-Davies <tgraydavies@gmail.com>
2020-12-14 23:06:53 +0000

    tidy day 12 solution

--- Day12/day12.py
+++ Day12/day12.py
@@ -1,66 +1,64 @@
 
-DIRECTIONS = ('N','E','S','W')
+from typing import List
+
+COMPASS = ('N','E','S','W')
 
 class Ship:
-    def __init__(self):
+    def __init__(self,rule):
         self.facing = 'E'
         self.pos = [0,0]
         self.waypoint = [10,1]
-    
+        self.rule = rule
+        
     def move(self,instruction: str):
         action,value = instruction[0],int(instruction[1:])
+        return getattr(self,'move'+str(self.rule))(action,value)
+    
+    def move1(self,action: str,value: int):
         if action == 'F':
             action = self.facing
 
         if action in ('L','S','W'):
             value = -1*value
 
-        if action in DIRECTIONS:
-            if action in ('N','S'):
-                self.pos[0] = self.pos[0]+value
-            else:
-                self.pos[1] = self.pos[1]+value
+        if action in COMPASS:
+            self.pos[(1+COMPASS.index(action)) % 2] += value
         else:
-            ix_dir = DIRECTIONS.index(self.facing)
-            self.facing = DIRECTIONS[(ix_dir + int(value/90)) % len(DIRECTIONS)]
+            ix_dir = COMPASS.index(self.facing)
+            self.facing = COMPASS[(ix_dir + value//90) % len(COMPASS)]
         return
 
-    def move2(self,instruction: str):
-        action,value = instruction[0],int(instruction[1:])
+    def move2(self,action: str,value: int):
         if action in ('L','S','W'):
             value = -1*value
-    
-        if action in DIRECTIONS:
+
+        if action in COMPASS:
             if action in ('N','S'):
-                self.waypoint[1] = self.waypoint[1]+value
-            else:
-                self.waypoint[0] = self.waypoint[0]+value
+                self.waypoint[(1+COMPASS.index(action)) % 2] += value
         elif action =='F':
-            self.pos[0] += int(value*self.waypoint[0])
-            self.pos[1] += int(value*self.waypoint[1])
+            self.pos = [x[0]+value*x[1] for x in zip(self.pos,self.waypoint)]
         else:
-            n_rot_right = int(value/90) % 4
-            if n_rot_right==1:
+            n_turn_clock = value//90 % 4
+            if n_turn_clock==1:
                 self.waypoint = [self.waypoint[1],-self.waypoint[0]]
-            elif n_rot_right==2:
+            elif n_turn_clock==2:
                 self.waypoint = [-self.waypoint[0],-self.waypoint[1]]
-            elif n_rot_right==3:
+            elif n_turn_clock==3:
                 self.waypoint = [-self.waypoint[1],self.waypoint[0]]
         return
+
+
+def manhattan_dist_travelled(directions: List[str],rule: int):
+    ship = Ship(rule)
+    for d in directions:
+        ship.move(d)
+    return sum(abs(p) for p in ship.pos)
 
 if __name__ == '__main__':
     
     with open("data.txt", "r") as f:
         directions = [c.rstrip('\n') for c in f.readlines()]
-    
-    ship = Ship()
-    for d in directions:
-        ship.move(d)
         
-    ship2 = Ship()
-    for d in directions:
-        ship2.move2(d)
-        
-    print('Manhattan distance from start is {0}'.format(sum(abs(p) for p in ship.pos)))
-    print('Manhattan distance from start using updated rules is {0}'.format(sum(abs(p) for p in ship2.pos))) 
+    print('Manhattan distance from start is {0}'.format(manhattan_dist_travelled(directions,1)))
+    print('Manhattan distance from start using updated rules is {0}'.format(manhattan_dist_travelled(directions,2)))
     
