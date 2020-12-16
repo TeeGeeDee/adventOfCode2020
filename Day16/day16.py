f96db9af8de3d7a37a2b9731c4986e40dd7ab88b
Tristan Gray-Davies <tgraydavies@gmail.com>
2020-12-16 22:54:39 +0000

    Tidy day 16 solution

--- Day16/day16.py
+++ Day16/day16.py
@@ -5,14 +5,18 @@ def is_valid(fld: int,rules: List[List[List[int]]]):
     all_rules = [item for sublist in rules for item in sublist]
     is_fld_valid = False
     for r in all_rules:
-        if r[0]<=fld<=r[1]:
-            is_fld_valid = True
+        is_fld_valid = r[0]<=fld<=r[1]
+        if is_fld_valid:
             break
     return is_fld_valid
 
-def get_valid_combo(valid_pos: dict):
+def get_valid_mapping(rule_names: List[str], rules: List[List[List[int]]],valid_tickets: List[int]):
+    valid_pos = {nm:set() for nm in rule_names}
+    for k in range(0,len(rules)):
+        for pos in range(0,len(valid_tickets[0])):
+            if all(is_valid(t[pos], [rules[k]]) for t in valid_tickets):
+                valid_pos[rule_names[k]].add(pos)
     out = {}
-    valid_pos = {k:set(valid_pos[k]) for k in valid_pos}
     while len(valid_pos)>0:
         for k in valid_pos:
             if len(valid_pos[k])==1:
@@ -22,12 +26,7 @@ def get_valid_combo(valid_pos: dict):
                 break
     return out
 
-
-if __name__ == '__main__':
-    
-    with open("data.txt", "r") as f:
-        data = [c.rstrip('\n') for c in f.readlines()]
-
+def unpack_data(data: List[str]):
     rules = []
     rule_names = []
     for d in data:
@@ -45,24 +44,20 @@ if __name__ == '__main__':
         other_tickets.append([int(d) for d in data[ii].split(',')])
     
     my_ticket = [int(d) for d in data[len(rules)+2].split(',')]
+    return rules,rule_names,my_ticket,other_tickets
+
+if __name__ == '__main__':
+    
+    with open("data.txt", "r") as f:
+        data = [c.rstrip('\n') for c in f.readlines()]
+
+    rules, rule_names, my_ticket, other_tickets = unpack_data(data)
     
     print('ticket scanning error rate is {0}'.format(sum(k for t in other_tickets for k in t if not is_valid(k,rules))))
 
     valid_tickets = [t for t in other_tickets if all(is_valid(f, rules) for f in t)]
-    
-    valid_pos = {nm:[] for nm in rule_names}
-    for k in range(0,len(rules)):
-        r = rules[k]
-        for i in range(0,len(valid_tickets[0])):
-            is_val = True
-            for j in range(0,len(valid_tickets)):
-                is_val &= is_valid(valid_tickets[j][i], [rules[k]])
-                if not is_val:
-                    break
-            if is_val:
-                valid_pos[rule_names[k]].append(i)
-    
-    out = get_valid_combo(valid_pos)
+    out = get_valid_mapping(rule_names, rules,valid_tickets)
+
     product = 1
     for f in out:
         if f.startswith('departure '):
