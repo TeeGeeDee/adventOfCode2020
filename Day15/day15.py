630abe928ef826ec5ebda69d07e201a182bfd41a
Tristan Gray-Davies <tgraydavies@gmail.com>
2020-12-23 17:26:38 +0000

    remove testing for day 15

--- Day15/day15.py
+++ Day15/day15.py
@@ -2,9 +2,6 @@
 from typing import Tuple
 
 DATA = (1,0,18,10,19,6)
-TESTS = {(0,3,6):436,(1,3,2):1,(2,1,3):10,(1,2,3):27,(2,3,1):78,(3,2,1):438,(3,1,2):1836}
-TESTS2 = {(0,3,6):175594,(1,3,2):2578,(2,1,3):3544142,(1,2,3):261214,(2,3,1):6895259, \
-          (3,2,1):18,(3,1,2):362}
 
 
 def get_nth_in_seq(start: Tuple[int],n: int):
@@ -22,12 +19,6 @@ def get_nth_in_seq(start: Tuple[int],n: int):
 
 if __name__ == '__main__':
 
-    for e in TESTS:
-        print('My answer = {0} vs given answer {1}'.format(get_nth_in_seq(e, 2020),TESTS[e]))
-    
     print('Part 1 2020th number called is {0}'.format(get_nth_in_seq(DATA, 2020)))
-    
-    for e in TESTS2:
-        print('My answer = {0} vs given answer {1}'.format(get_nth_in_seq(e, 30000000),TESTS2[e]))
     
     print('Part 2 30000000 number called is {0}'.format(get_nth_in_seq(DATA, 30000000)))
