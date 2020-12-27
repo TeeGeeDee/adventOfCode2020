494e9378c3ce15da9eb6cb18e9210743a18c1f6a
Tristan Gray-Davies <tgraydavies@gmail.com>
2020-12-27 18:11:58 +0000

    tiday day 21 solution

--- Day21/day21.py
+++ Day21/day21.py
@@ -4,26 +4,26 @@ if __name__ == '__main__':
     with open("data.txt", "r") as f:
         data = [c.rstrip('\n') for c in f.readlines()]
     
-    recipies = []
+    allergens_map = {}
+    ingredients_all = []
     for row in data:
         ingredients,_,allergens = row.rstrip(')').partition(' (contains ')
-        recipies += [(set(ingredients.split(' ')),set(allergens.split(', ')))]
-    recipies = tuple(recipies)
-    
-    allergens_map = {}
-    for rec in recipies:
-        for a in rec[1]:
+        ingredients = set(ingredients.split(' '))
+        ingredients_all += ingredients
+        allergens = set(allergens.split(', '))
+        for a in allergens:
             if a not in allergens_map:
-                allergens_map[a] = rec[0].copy()
+                allergens_map[a] = ingredients.copy()
             else:
-                allergens_map[a].intersection_update(rec[0])
+                allergens_map[a].intersection_update(ingredients)
     taken_ingredients = tuple(list(ing)[0] for ing in allergens_map.values() if len(ing)==1)
     to_rm = taken_ingredients
     while not all(len(ing)==1 for ing in allergens_map.values()):
         [allergens_map[aller].discard(rm) for rm in to_rm for aller in allergens_map if len(allergens_map[aller])>1]
         to_rm = tuple(set(list(ing)[0] for ing in allergens_map.values() if len(ing)==1).difference(taken_ingredients))
     
-    print('Part 1 answer = {0}'.format(sum(len(r[0].difference([list(a)[0] for a in allergens_map.values()])) for r in recipies)))
     ingredients_map = {list(allergens_map[aller])[0]:aller for aller in allergens_map}
+
+    print('Part 1 answer = {0}'.format(sum(ing not in ingredients_map for ing in ingredients_all)))
     print('Part 2 answer = {0}'.format(','.join(sorted(ingredients_map.keys(),key=lambda x: ingredients_map[x]))))
         
