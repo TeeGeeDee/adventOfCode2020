e1b093e090cb75df1cf092b718f5d4ac46b50d6a
Tristan Gray-Davies <tgraydavies@gmail.com>
2020-12-29 02:34:57 +0000

    Tidy day 24

--- Day24/day24.py
+++ Day24/day24.py
@@ -2,11 +2,15 @@
 from typing import List
 from collections import Counter
 
-NEIGHBOURS = ((2,0),(1,1),(-1,1),(-2,0),(-1,-1),(1,-1))
+def canonical_form(mv: Counter):
+    return (2*mv['e']+mv['ne']+mv['se']-2*mv['w']-mv['nw']-mv['sw'], mv['ne']+mv['nw']-mv['se']-mv['sw'])
 
-def flip_tiles(data: List[str]):
+def neighbours(tile):
+    return [(tile[0]+mv[0],tile[1]+mv[1]) for mv in ((2,0),(1,1),(-1,1),(-2,0),(-1,-1),(1,-1))]
+
+def flip_tiles(flip_list: List[str]):
     black_tiles = set()
-    for row in data:
+    for row in flip_list:
         moves = Counter()
         for ix in range(len(row)):
             if row[ix] in ('n','s'):
@@ -16,39 +20,31 @@ def flip_tiles(data: List[str]):
             else:
                 direction = row[ix]
             moves[direction] += 1
-        canonical_form = (2*moves['e']+moves['ne']+moves['se']-(2*moves['w']+moves['nw']+moves['sw']),
-                                                                  moves['ne']+moves['nw']-(moves['se']+moves['sw']))
-        if canonical_form in black_tiles:
-            black_tiles.remove(canonical_form)
+        tile = canonical_form(moves)
+
+        if tile in black_tiles:
+            black_tiles.remove(tile)
         else:
-            black_tiles.add(canonical_form)
+            black_tiles.add(tile)
     return black_tiles
 
 def day_update(black_tiles: set):
-    flip_to_white = set()
-    for tile in black_tiles:
-        num_black_neighbours = sum((tile[0]+mv[0],tile[1]+mv[1]) in black_tiles for mv in NEIGHBOURS)
-        if num_black_neighbours == 0 or num_black_neighbours > 2:
-            flip_to_white.add(tile)
-    flip_to_black = set()
-    all_neighbours = tuple((tile[0]+mv[0],tile[1]+mv[1]) for mv in NEIGHBOURS for tile in black_tiles)
-    for tile in all_neighbours:
-        if sum((tile[0]+mv[0],tile[1]+mv[1]) in black_tiles for mv in NEIGHBOURS) == 2:
-            flip_to_black.add(tile)
+    flip_to_white = [tile for tile in black_tiles if len(black_tiles.intersection(neighbours(tile))) not in (1,2)]
+    white_tiles = set(tile for bt in black_tiles for tile in neighbours(bt) if tile not in black_tiles)
+    flip_to_black = [tile for tile in white_tiles if len(black_tiles.intersection(neighbours(tile))) == 2]
     black_tiles.difference_update(flip_to_white)
     black_tiles.update(flip_to_black)
     return black_tiles
 
-
 if  __name__ == '__main__':
     
     with open('data.txt') as f:
-        data = [r.rstrip('\n') for r in f]
+        flip_list = [r.rstrip('\n') for r in f]
     
-    black_tiles = flip_tiles(data)
-    print('Part 1 ends up with {0} black tiles'.format(len(black_tiles)))
+    black_tiles = flip_tiles(flip_list)
+    print('Part 1 has {0} black tiles'.format(len(black_tiles)))
     
-    for ii in range(100):
+    for _ in range(100):
         day_update(black_tiles)
-    print('Part 2 ends up with {0} black tiles'.format(len(black_tiles)))
+    print('Part 2 has {0} black tiles'.format(len(black_tiles)))
         
