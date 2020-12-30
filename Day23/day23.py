5d5555d17792c28ce59135a7b15c6dc0a5319cb7
Tristan Gray-Davies <tgraydavies@gmail.com>
2020-12-30 13:56:37 +0000

    move to standard linked list for simplicity and speed

--- Day23/day23.py
+++ Day23/day23.py
@@ -3,15 +3,15 @@ INPUT = '942387615'
 N_PICK_OUT = 3
 
 def play_game(str_in,set_size,n_moves):
-    doubly_linked_list = {}
+    linked_list = {}
     for ix in range(set_size):
         ix += 1
-        doubly_linked_list[get_num(ix,set_size)] = [get_num(ix-1,set_size),get_num(ix+1,set_size)]
+        linked_list[get_num(ix,set_size)] = get_num(ix+1,set_size)
     
     current = int(str_in[0])
     for ii in range(n_moves):
-        current = move(current,doubly_linked_list,set_size)
-    return doubly_linked_list
+        current = move(current,linked_list,set_size)
+    return linked_list
 
 def mod_end(a,b):
     out = a % b
@@ -27,41 +27,35 @@ def get_num(ix,set_size):
     else:
         return get_num(mod_end(ix,set_size),set_size)
     
-def move(current,d_ll,set_size):
+def move(current,ll,set_size):
     pick_out = []
     x0 = current
     for ii in range(N_PICK_OUT):
-        x0 = d_ll[x0][1]
+        x0 = ll[x0]
         pick_out.append(x0)
     destination = mod_end(current-1,set_size)
     while destination in pick_out:
         destination = mod_end(destination-1,set_size)
-    # current to where picked_out pointed to
-    d_ll[current][1] = d_ll[pick_out[2]][1]
-    d_ll[d_ll[current][1]][0] = current
-    # picked out end to where destination pointed to
-    d_ll[pick_out[-1]][1] = d_ll[destination][1]
-    d_ll[d_ll[pick_out[-1]][1]][0] = pick_out[-1]
-    # destination to picked_out start
-    d_ll[destination][1] = pick_out[0]
-    d_ll[pick_out[0]][0] = destination
+    ll[current]      = ll[pick_out[2]]
+    ll[pick_out[-1]] = ll[destination]
+    ll[destination]  = pick_out[0]
 
-    return d_ll[current][1]
+    return ll[current]
     
 
 if __name__ == '__main__':
     
-    d_ll_1 = play_game(INPUT,9,100)
+    ll_1 = play_game(INPUT,9,100)
     out = []
-    current = d_ll_1[1][1]
+    current = ll_1[1]
     while current != 1:
         out.append(current)
-        current = d_ll_1[current][1]
+        current = ll_1[current]
     print('Part 1 answer = {0}'.format(''.join(str(y) for y in out)))
     
-    d_ll_2 = play_game(INPUT,1000000,10000000)
+    ll_2 = play_game(INPUT,1000000,10000000)
     
-    x1 = d_ll_2[1][1]
-    x2 = d_ll_2[x1][1]
+    x1 = ll_2[1]
+    x2 = ll_2[x1]
     print('Part 2 answer = {0}'.format(x1*x2))
    
